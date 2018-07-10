baa4e915eb2188c79b1cf874733c75cab266df3f
ark120202 <ark120202@gmail.com>
2018-07-10 18:25:57 +0500

    chore(frontend): remove commented out code

--- frontend/src/components/Footer/Footer.vue
+++ frontend/src/components/Footer/Footer.vue
@@ -1,33 +1,6 @@
 <template>
   <footer class="footer">
     <div class="container has-text-centered">
-      <!-- <div class="tabs is-centered">
-        <ul>
-          <li>
-              <a href="https://github.com/ark120202/easy-elo-ranker">Terms</a>
-          </li>
-        </ul>
-        <ul>
-          <li>
-              <a href="https://github.com/ark120202/easy-elo-ranker">About Us</a>
-          </li>
-        </ul>
-        <ul>
-          <li>
-              <a href="https://github.com/ark120202/easy-elo-ranker">API</a>
-          </li>
-        </ul>
-        <ul>
-          <li>
-              <a href="https://github.com/ark120202/easy-elo-ranker">Github</a>
-          </li>
-        </ul>
-        <ul>
-          <li>
-              <a href="https://github.com/ark120202/easy-elo-ranker">Discord</a>
-          </li>
-        </ul>
-      </div> -->
       <a href="https://bulma.io">
         <img src="~@/assets/bulma.png" alt="Made with Bulma" width="128" height="24">
       </a>
